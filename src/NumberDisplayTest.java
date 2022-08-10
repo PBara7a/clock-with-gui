97e3545b3119630b9d4f00a645a2b735601496a9
Paulo Barata <pbara7a@gmail.com>
2022-08-10 16:27:42 +0100

    NumberDisplay tests.

---
+++ src/NumberDisplayTest.java
@@ -0,0 +1,40 @@
+package src;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class NumberDisplayTest {
+  @Test
+  public void testgetValue() {
+    NumberDisplay display = new NumberDisplay(60);
+
+    assertEquals(0, display.getValue());
+  }
+
+  @Test
+  public void testSetValue() {
+    NumberDisplay display = new NumberDisplay(60);
+    display.setValue(35);
+
+    assertEquals(35, display.getValue());
+  }
+
+  @Test
+  public void testIncrement() {
+    NumberDisplay display = new NumberDisplay(60);
+    display.increment();
+    display.increment();
+
+    assertEquals(2, display.getValue());
+  }
+
+  @Test
+  public void testGetDisplayValue() {
+    NumberDisplay display = new NumberDisplay(60);
+    display.increment();
+    display.increment();
+
+    assertEquals("02", display.getDisplayValue());
+  }
+}
